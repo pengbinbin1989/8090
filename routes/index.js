275f950eea6dff8a82b6aace99aa350fb1f043d1
pengbinbin1989 <475731530@qq.com>
2016-03-01 17:24:20 +0800

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var request = require('request');
+var cityWeatherCode = require('cityWeatherCode').China.province;
 var router = express.Router();
 
 /* GET home page. */
@@ -18,53 +19,81 @@ router.get('/', function(req, res, next) {
 });
 
 
-/* getWeather by latlng */
+/* getWeather by cityCode */
+/*
+ * 获取设备IP、经纬度
+ * 获取可查询地点的城市代码
+ * 获取相关信息
+*/
     router.post('/', function(req, res, next) {
-        req.body.latlng ? getCityName(req.body.latlng,res,0) : getCityName(req.body.IP,res,1);
+        req.body.latlng ? getCityName(req.body.latlng,res,0) : getCityCode(req.body.province, req.body.cityName, req.body.district, req.body.address, res);
     });
     var def = {
-        getCityNameUrl_:'http://maps.google.cn/maps/api/geocode/json?language=EN&latlng=',
-        getCityNameByIPUrl_:'http://apis.baidu.com/apistore/iplookupservice/iplookup?ip=',
-        getWeather_:"http://apis.baidu.com/apistore/weatherservice/weather?citypinyin=",
+        getCityNameUrl_:'http://api.map.baidu.com/geocoder?output=json&location=',
+        getWeather_:"http://apis.baidu.com/apistore/weatherservice/cityname?cityname=",
         getWeatherOptions:{
             url:null,
             headers:{ apikey:'fd6b4fe2716d5647dc09b9ac034a1de1' }
         }
     };
     function getCityName(_url, res, status){
-        if (status == 0) {
-            request(def.getCityNameUrl_ + _url, function(error, request, body) {
-                if (!error && request.statusCode == 200) {
-                    typeof(body) == "string" ? body = JSON.parse(body) : void(0);
-                    var address = body.results[0].formatted_address;
-                    var re = /(\,\s)[\w\W][^\,]*(?=\sshi\,|\sShi\,)/;
-                    var addr = re.exec(address);
-                    if (addr == null) {
-                        addr = address.replace(/\s/g,"").split(",");
-                        getWeather(addr[addr.length - 1],res);
-                    }else{
-                        var cityName = addr[0].replace(", ","");
-                        getWeather(cityName,res);
-                    }
+        request(def.getCityNameUrl_ + _url, function(error, request, body) {
+            if (!error && request.statusCode == 200) {
+                typeof(body) == "string" ? body = JSON.parse(body) : void(0);
+                if (body.status == "OK") {
+                    getCityCode(body.result.addressComponent.province, body.result.addressComponent.city, body.result.addressComponent.district, body.result.formatted_address, res)
                 };
-            });
-        }else if (status == 1) {
-            def.getWeatherOptions.url = def.getCityNameByIPUrl_ + _url;
-            request(def.getWeatherOptions, function(error, request, body) {
-                if (!error && request.statusCode == 200) {
-                    typeof(body) == "string" ? body = JSON.parse(body) : void(0);
-                    def.getWeather_ = 'http://apis.baidu.com/apistore/weatherservice/cityname?cityname=';
-                    getWeather(encodeURI(body.retData.city), res);
-                }
-            });
-        } 
+            };
+        });
     };
-        function getWeather(cityName, res){
-            def.getWeatherOptions.url = def.getWeather_ + cityName;
-            request(def.getWeatherOptions, function(error, request, body) {
-                if (!error && request.statusCode == 200) {console.log();res.end(body);}
-            });
-        };
+
+    function getCityCode(province, cityName, district, address, res){
+        for (var i = 0; i < cityWeatherCode.length; i++) {
+            if (province.indexOf(cityWeatherCode[i].name) >= 0) {
+                var city = cityWeatherCode[i].city;
+                if (city.length) {
+                    for (var j = 0; j < city.length; j++) {
+                        if (cityName.indexOf(city[j].name) >= 0) {
+                            var county = city[j].county;
+                            for (var k = 0; k < county.length; k++) {
+                                if (district.indexOf(county[k].name) >= 0) {
+                                    res.send(JSON.stringify({"address": address ,"cityCode": county[k].code}));
+                                    res.end();
+                                    break;
+                                }else if (k === county.length - 1) {
+                                    var newCounty = county;
+                                    for (var q = 0; q < district.length; q++) {
+                                        (function(q){
+                                            newCounty = re(newCounty,district[q]);
+                                        })(q);
+                                        if (newCounty.length === 1) {break};
+                                    };
+                                    res.send(JSON.stringify({"address": address ,"cityCode": newCounty[0].code}));
+                                    res.end();
+                                    function re(newarr,req) {
+                                        var new_ = [];
+                                        for (var l = 0; l < newarr.length; l++) {
+                                            if (newarr[l].name.indexOf(req) >= 0) {
+                                                new_.push(newarr[l]);
+                                            }
+                                        };
+                                        if (new_.length === 0) {new_ = newarr;}
+                                        return new_;
+                                    }
+                                }
+                            }
+                            break;
+                        }
+                    }
+                }else{
+                    var county = city.county;
+                    res.send(JSON.stringify({"address": address ,"cityCode": county[0].code}));
+                    res.end();
+                }
+                break;
+            }
+        }
+    };
 /* getWeather by latlng */
 
 module.exports = router;
